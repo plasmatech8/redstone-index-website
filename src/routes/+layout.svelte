84d866ebdedb05d9b621b9dfa515cfed0b0b0879
Mark Connelly <com0004@gmail.com>
2022-11-02 21:09:20 +1100

    Install tailwind properly

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	import '../app.css';
+	import {
+		SvelteUIProvider,
+		fns,
+		AppShell,
+		Navbar,
+		Header,
+		Title,
+		Divider
+	} from '@svelteuidev/core';
+	import { isDarkTheme } from '$lib/stores';
+
+	function toggleTheme() {
+		$isDarkTheme = !$isDarkTheme;
+	}
+</script>
+
+<SvelteUIProvider
+	ssr
+	withNormalizeCSS
+	withGlobalStyles
+	themeObserver={$isDarkTheme ? 'dark' : 'light'}
+>
+	<AppShell>
+		<Header slot="header" height="100">
+			<nav class="flex bg-yellow-500">
+				<!-- left -->
+				<div class="bg-red-500">Left</div>
+				<div>Middle</div>
+				<div>Right</div>
+			</nav>
+		</Header>
+		<slot>This is the main content</slot>
+	</AppShell>
+</SvelteUIProvider>
