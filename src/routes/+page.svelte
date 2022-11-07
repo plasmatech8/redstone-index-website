d1bd3dcbf9d6f9d5bc864748b811a4cc843bf527
Mark Connelly <com0004@gmail.com>
2022-11-07 22:49:24 +1100

    Add main page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Grid, Container, Button, Group, NativeSelect, Modal, Badge } from '@svelteuidev/core';
+	import { Person, Stack } from 'radix-icons-svelte';
+	let opened = false;
+</script>
+
+<Group position="center" class="mb-5">
+	<Button on:click={() => (opened = true)} color="red">
+		<i class="fa-solid fa-user" slot="leftIcon" />
+		Author
+	</Button>
+	<Button on:click={() => (opened = true)} color="red">
+		<i class="fa-solid fa-layer-group" slot="leftIcon" />
+		Type
+	</Button>
+	<Button on:click={() => (opened = true)} color="red">
+		<i class="fa-solid fa-tag text-white" slot="leftIcon" />
+		Tags
+	</Button>
+</Group>
+
+<Group position="center" class="mb-5">
+	<Badge>Popular last 7 days</Badge>
+</Group>
+
+<Group position="center">
+	{#each Array(30) as _}
+		<div class="bg-red-500 h-72 w-72">A</div>
+	{/each}
+</Group>
+
+<Modal {opened} on:close={() => (opened = false)} title="Introduce yourself!">Hello World!</Modal>
---
+++ src/routes/TempCard.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Card, Text, Badge, Group } from '@svelteuidev/core';
+</script>
+
+<Card p="lg">
+	<Card.Section padding="lg">
+		<img
+			src="https://images.unsplash.com/photo-1555881400-74d7acaacd8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80"
+			height={160}
+			alt="Portugal"
+		/>
+	</Card.Section>
+	<Group position="apart">
+		<Text weight={500}>Portugal Porto Adventures</Text>
+		<Badge color="pink" variant="light">On Sale</Badge>
+	</Group>
+	<Text size="sm">
+		With Portugal Porto Adventures you can explore more of the beautiful portuguese cities, by
+		walking on food, meeting the locals and eat excellent food and wine
+	</Text>
+</Card>
